0f5eb004875ea3edf50e205b444b262c5d01d8f9
I-yan-u <iyanuajimobi12@gmail.com>
2024-04-17 19:14:21 +0100

    Add /status API endpoint to check server status

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ app.use(cors());
 const port = process.env.PORT;
 
 app.get("/", (req, res) => res.send("Mailer app is running!"));
+app.get("/status", (req, res) => res.status(200).send({ status: "ok" }));
 
 app.listen(port, () => console.log(`Server ready on port ${port}.`));
 
