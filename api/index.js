fb037ba5a9414fad661e2bcec509bbc715664a0b
Iyanu Ajimobi <iyanuajimobi12@gmail.com>
2025-09-15 13:22:56 +0100

    Refactor code structure for improved readability and maintainability

--- api/index.js
+++ api/index.js
@@ -13,52 +13,90 @@ app.use(cors());
 
 // Environment variables
 const port = process.env.PORT;
-const user = process.env.USER;
-const pass = process.env.PASS;
+const username = process.env.USER;
+const password = process.env.PASS;
 const to = process.env.RECEIVER || 'iyanuajimobi12@gmail.com';
 
 // Nodemailer transporter
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
+const createTransporter = ({
+  user = username,
+  pass = password,
+  host,
+  port,
+  type = 'gmail',
+}) => {
+  if (type === 'gmail') {
+    return nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
         user,
         pass,
-    },
-});
+      },
+    });
+  } else {
+    return nodemailer.createTransport({
+      host,
+      port,
+      secure: true,
+      auth: {
+        user,
+        pass,
+      },
+    });
+  }
+};
 
 // App route to test if the app is running
-app.get("/", (_, res) => res.status(200).send({message: "Mailer app is running!"}));
+app.get('/', (_, res) =>
+  res.status(200).send({ message: 'Mailer app is running!' })
+);
 
 // App route to check status of the app
-app.get("/status", (_, res) => res.status(200).send({ status: "ok", timestamp: new Date(), 'emote': '🚀'}));
+app.get('/status', (_, res) =>
+  res.status(200).send({ status: 'ok', timestamp: new Date(), emote: '🚀' })
+);
 
 // App route to send an email
-app.post("/send", async (req, res) => {
-    let { sender, subject, text, name } = req.body;
+app.post('/send', async (req, res) => {
+  let { sender, subject, text, name, to, config } = req.body;
+  const { user, pass, host, port, type } = config || {};
 
-    if (!sender || !subject || !text) {
-        return res.status(400).send({ message: "Missing required fields." });
-    }
+  if (!sender || !subject || !text) {
+    return res.status(400).send({ message: 'Missing required fields.' });
+  }
 
-    if (!name) {
-        text = text;
-    } else {
-        text = `New Message from ${name},\n\n${text}`;
-    }
+  if (!name) {
+    text = text;
+  } else {
+    text = `New Message from ${name},\n\n${text}`;
+  }
 
-    try{
-        const mailOptions = {
-            from: user,
-            to,
-            cc: sender,
-            subject: subject,
-            text: text
-        };
-        const info = await transporter.sendMail(mailOptions);
-        return res.status(200).send({ message: "Email sent.", messageId: info.messageId });
-    } catch (error) {
-        return res.status(500).send({ message: "Internal server error." });
-    }
+  try {
+    const transporter = createTransporter({ user, pass, host, port, type });
+    transporter.verify((error, success) => {
+      if (error) {
+        console.log(error);
+        return res.status(400).send({ message: 'Invalid configuration.' });
+      }
+    });
+
+    const mailOptions = {
+      from: user,
+      to,
+      cc: sender,
+      subject: subject,
+      text: text,
+    };
+    const info = await transporter.sendMail(mailOptions);
+    return res
+      .status(200)
+      .send({ message: 'Email sent.', messageId: info.messageId });
+  } catch (error) {
+    return res.status(500).send({ message: 'Internal server error.' });
+  } finally {
+    console.log('Email send attempt finished.');
+    transporter.close();
+  }
 });
 
 // Initialize the server
